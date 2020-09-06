c1d030b37c7486538fc568f43c78e0fb123156d5
Karthik <karthikthevally@gmail.com>
2020-09-06 21:21:15 +0530

    Updates

---
+++ precache-manifest.290843cc697732826c4cf9c09e8744a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554690244b7690fd62de54fb49cf2df5",
+    "url": "/robo-friends-app/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robo-friends-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d9c10162695fa25ea009",
+    "url": "/robo-friends-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robo-friends-app/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends-app/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c10162695fa25ea009",
+    "url": "/robo-friends-app/static/js/main.a2d6336e.chunk.js"
+  },
+  {
+    "revision": "c95c68944cdafda08f45",
+    "url": "/robo-friends-app/static/js/runtime-main.1b207403.js"
+  }
+]);
